89eb093fccf71a7edb023259d2f949701f0bc9e2
rachelaleone7@gmail.com <rachelleone7@gmail.com>
2022-02-26 14:30:12 -0500

    commits for both

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 
+
 function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.writeHead(500, {'Content-Type': 'text/html'});
     fs.readFile('./index.ejs', null, function(error, data) {
         if (error) {
             response.writeHead(404);
@@ -13,5 +14,6 @@ function onRequest(request, response) {
         response.end();
     },)
 }
+
 
 http.createServer(onRequest).listen(8080);
