8408a2e552791dac2b809829a4c9052a349f4c3b
apk9574 <apk9574@rit.edu>
2022-02-27 02:58:32 -0500

    got the image display working, ish

--- server.js
+++ server.js
@@ -12,12 +12,17 @@ app.get('/', (req, res) => {
 
 app.get('/search', bodyParser, (req, res) => {
     var item = req.query.search;
-    console.log(item);
-    request("http://localhost:5000", { json: true }, (err, res, body) => {
+    request("http://localhost:5000/" + item, { json: true }, (err, r, body) => {
         if (err) { return console.log(err); }
-        console.log(res.body);
-});
-    res.render("index", {display: res.body});
+        var d = "none";
+        if (r.body == "Positive") {
+            d = "Positive";
+        }
+        else if (r.body == "Negative") {
+            d = "Negative";
+        }
+        res.render("index", {display: d});
+    });
 });
 
 port = 8080
@@ -27,4 +32,3 @@ app.listen(port, () => {
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
-
