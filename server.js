adb534c91e2643794fb3099ae958062a97c6b891
rachelaleone7@gmail.com <rachelleone7@gmail.com>
2022-02-26 20:00:53 -0500

    added a search route to figure out what the user typed into the search bar

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var http = require('http');
 var fs = require('fs');
+const bodyParser = require('body-parser').urlencoded({extended: true});
 
 var express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
     res.render("index");
+});
+
+app.get('/search', bodyParser, (req, res) => {
+    var item = req.query.search;
+    console.log(item);
+    res.redirect("/");
 });
 
 port = 8080
